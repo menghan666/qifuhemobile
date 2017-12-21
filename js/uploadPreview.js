7b4d1240ca1faec7f6c58ccacd1865ba865af943
Administrator <Administrator@PC-20131207VBMC>
2017-12-21 17:32:15 +0800

    个人中心-修改密码、修改手机号、我的公司

--- js/uploadPreview.js
+++ js/uploadPreview.js
@@ -2,7 +2,7 @@
 *名称:图片上传本地预览插件 v1.1
 *作者:周祥
 *时间:2013年11月26日
-*介绍:基于JQUERY扩展,图片上传预览插件 目前兼容浏览器(IE 谷歌 火狐) 不支持safari
+*介绍:基于JQUERY扩展,图片上传预览插件 目前兼容浏览器(IE 谷歌 火狐 safari)
 *插件网站:http://keleyi.com/keleyi/phtml/image/16.htm
 *参数说明: Img:图片ID;Width:预览宽度;Height:预览高度;ImgType:支持文件类型;Callback:选择文件显示图片后回调方法;
 *使用方法: 
